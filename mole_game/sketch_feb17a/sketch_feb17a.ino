9538362f4b1052c4f29b4be749312f10e88f3e79
Arjun Singh <arjunsingh@ucsb.edu>
2020-02-19 07:33:21 -0800

    added score and timer

--- mole_game/sketch_feb17a/sketch_feb17a.ino
+++ mole_game/sketch_feb17a/sketch_feb17a.ino
@@ -48,9 +48,24 @@ int getHit(int a){
   if (digitalRead(a) == 1)
     return 1;
   else
-    return 10;
+    return 2;
 }
 
+int numPlaces (int n) {
+    if (n < 10) return 1;
+    if (n < 100) return 2;
+    if (n < 1000) return 3;
+}
+
+void timerPrint(int a){
+  int numSize = numPlaces(a);
+
+  lcd.setCursor(15 - numSize,1);
+  lcd.print(" ");
+  lcd.setCursor(16 - numSize,1);
+  lcd.print(a);
+  
+}
 
 void begin_game(){
   Serial.println("Beginning game \n");
@@ -61,16 +76,33 @@ void begin_game(){
   lcd.print("Set...");
   delay(1000);
   lcd.clear();
-  lcd.print("GO");
+  lcd.print("GO!!!");
   delay(1000);
+  lcd.clear();
+}
+
+void end_game(){
+  Serial.println("Ending game");
+  lcd.clear();
+  lcd.print("Time's up!");
   lcd.clear();
 }
 
 void whack_a_mole(){
            
   begin_game();
- 
-  while(true){
+
+  int timer = 20;
+
+  int score = 0;
+  lcd.setCursor(0,0);
+  lcd.print("Score:");
+  lcd.setCursor(0,1);
+  lcd.print(score);
+  lcd.setCursor(16 - numPlaces(timer),1);
+  lcd.print(timer);
+  
+  while(timer > 0){
     int arr[]={0,0,0,0}; 
     int rand_num1 = (int) random(0,4);
     int control = (int) random(1,11);
@@ -78,20 +110,26 @@ void whack_a_mole(){
     
     if (control >= 8)
       rand_num2 = (int) random(0,4);
-    else
+    else  
       rand_num2 = rand_num1;
       
     if (arr[rand_num1] == 0)
       arr[rand_num1] = 1;
 
     if (arr[rand_num2] == 0)
-      arr[rand_num2] = 1;
-    
+      arr[rand_num2] = 1;  
+
+    for (int i=0;i<4;i++)
+      Serial.print(arr[i]);
+    Serial.print("\n");
       for (int pin=3;pin<=6;pin++){
         
         if (getHit(pin) == arr[pin-3]){
-          Serial.println("Mole hit on pin: " + (String)pin);for (int i=0;i<4;i++)
+          Serial.println("Mole hit on pin: " + (String)pin);
 
+          score += 1;
+          arr[pin-3] = 0;
+          
           for (int i=0;i<4;i++)
             Serial.print(arr[i]);
           Serial.print("\n");
@@ -100,11 +138,29 @@ void whack_a_mole(){
       }
       
     
+    lcd.setCursor(0,1);
+    lcd.print(score);
     
-
-    delay(3000);
-
+    
+    delay(1000);
+    timer -= 1;
+    timerPrint(timer);
+    delay(1000);
+    timer -= 1;
+    timerPrint(timer);
+    delay(1000);    
+    timer -= 1;
+    timerPrint(timer);
+    
   }
+
+  lcd.clear();
+  lcd.print("GAME OVER");
+  delay(2000);
+  lcd.clear();
+  lcd.print("Your score was:");
+  lcd.setCursor(0,1);
+  lcd.print(score);
   
 }
  
